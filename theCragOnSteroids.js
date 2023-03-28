4a1c2d66a8cf628b99781ad2468c48201952c807
Thomas Heuberger <t-h@t-h.cc>
2023-03-28 17:27:18 +0200

    Create theCragOnSteroids.js

---
+++ theCragOnSteroids.js
@@ -0,0 +1,71 @@
+// ==UserScript==
+// @name         theCrag on steroids
+// @namespace    http://tampermonkey.net/
+// @version      0.2.1
+// @description  Some userspace impovements to make theCrag even better!
+// @author       Thomas Heuberger
+// @match        https://www.thecrag.com/*/routes*
+// @icon         https://static.thecrag.com/cids/images/logo17-1.1.1.svg
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    const routesTable = document.getElementsByClassName("routetable facet-results")[0];
+    routesTable.id = "resultTable";
+    const cragsByGroup = routesTable.getElementsByClassName("group");
+    const header = routesTable.querySelector("tbody > tr:nth-child(1) > th:nth-child(4)");
+    const routesRows = routesTable.querySelector("tbody").getElementsByTagName("tr");
+
+    header.appendChild(document.createTextNode(`   (Displaying ${cragsByGroup.length} crag${cragsByGroup.length > 1 ? 's' : ''} on this page)`));
+
+    const routesByCragMap = new Map();
+    let currentSector = "";
+    let currentCrag = "";
+
+    function GetCurrentCrag(selector) {
+        return selector.length > 1 ? selector[1].querySelector("a").text : "";
+    }
+
+    function GetCurrentSector(selector) {
+        return selector[0].querySelector("a").text;
+    }
+
+    Array.from(routesRows).forEach((el, index) => {
+        if (index != 0) {
+            if (el.getElementsByClassName("crumbtrail-partial__crumb nowrap").length != 0) {
+                const thisSelector = el.getElementsByClassName("crumbtrail-partial__crumb nowrap");
+                currentSector = GetCurrentSector(thisSelector);
+                currentCrag = GetCurrentCrag(thisSelector);
+            } else {
+                if (!routesByCragMap.has(currentSector)) {
+                    const thisCrag = new Map();
+                    thisCrag.set(currentCrag, {routes: 1});
+                    routesByCragMap.set(currentSector, {totalRoutes: 1, Crags:  thisCrag});
+                } else {
+                    routesByCragMap.get(currentSector).totalRoutes += 1;
+                    if (!routesByCragMap.get(currentSector).Crags.has(currentCrag)) {
+                        routesByCragMap.get(currentSector).Crags.set(currentCrag, {routes: 1});
+                    } else {
+                        routesByCragMap.get(currentSector).Crags.get(currentCrag).routes += 1;
+                    }
+                }
+            }
+        }
+    });
+
+    Array.from(cragsByGroup).forEach((el, index) => {
+        let countDiv = document.createElement("div");
+        countDiv.style = "display: inline; float: right;";
+        const thisSelector = el.getElementsByClassName("crumbtrail-partial__crumb nowrap");
+        currentSector = GetCurrentSector(thisSelector);
+        currentCrag = GetCurrentCrag(thisSelector);
+        const inSector = `${routesByCragMap.get(currentSector).totalRoutes} route${routesByCragMap.get(currentSector).totalRoutes > 1 ? "s" : ""} in sector ${currentSector}`;
+        const inCrag = `${routesByCragMap.get(currentSector).Crags.get(currentCrag).routes} route${routesByCragMap.get(currentSector).Crags.get(currentCrag).routes > 1 ? "s": ""} in crag ${currentCrag}`;
+        countDiv.textContent = currentCrag != "" ? `   (${inSector}, ` : "";
+        countDiv.textContent += `${inCrag}`;
+        countDiv.textContent += "on this page)";
+        el.appendChild(countDiv);
+    }, routesByCragMap);
+})();
